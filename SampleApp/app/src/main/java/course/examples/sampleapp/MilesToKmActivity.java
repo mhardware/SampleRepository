cda36b2bc6d4fb8ec8d4ce48e0679fbdc7c8a6b7
mhardware <mhardware@hotmail.com>
2016-06-30 10:04:30 +0100

    Added sample app
    Added sample app to repository.

---
+++ SampleApp/app/src/main/java/course/examples/sampleapp/MilesToKmActivity.java
@@ -0,0 +1,13 @@
+package course.examples.sampleapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MilesToKmActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_miles_to_km);
+    }
+}
---
+++ SampleApp/app/src/test/java/course/examples/sampleapp/ExampleUnitTest.java
@@ -0,0 +1,27 @@
+package course.examples.sampleapp;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+import java.util.regex.Pattern;
+// import static org.junit.Assert.assertFalse;
+// import static org.junit.Assert.assertTrue;
+
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+    
+    @Test
+    public void testExampleTest() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+    
+}
